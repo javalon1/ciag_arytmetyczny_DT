390f706cc037b5613834cab167ad8a94f2ef09cb
Damian Tatarowicz <damian.tatarowicz@gmail.com>
2019-05-18 12:17:20 +0100

    gotowy kod na ciag

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,26 @@
 package com.sda.algorytmy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("podaj liczbe wyrazow: ");
+        int n = scanner.nextInt();
+        System.out.println("podaj pierwszy wyraz: ");
+        int a1 = scanner.nextInt();
+        System.out.println("podaj przyrost: ");
+        int r = scanner.nextInt();
+        System.out.println(a1);
+
+        // (a1 + (i -1) * r)
+
+        for (int i = 2; i <= n; i++) {
+
+            System.out.println((a1 + (i - 1) * r));
+
+        }
+
     }
 }
+
